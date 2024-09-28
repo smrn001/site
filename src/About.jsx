9768ad71cdf2287070a223c1f67893f5dd38372e
smrn001 <samir.niroula001@gmail.com>
2024-09-28 12:56:04 +0545

    Refactor project cards in Projects component

--- src/About.jsx
+++ src/About.jsx
@@ -15,16 +15,16 @@ const About = () => {
         <p>into technology, and making waves in the ever-evolving IT world.</p>
       </div>
       <div className="flex flex-row justify-between text-white space-x-3 items-center">
-      <button
-  type="button"
-  className="border-2 text-primary border-primary p-3 min-w-32 text-center bg-transparent relative overflow-hidden hover:text-white transition-colors hover:z-[2] duration-300 group font-bold cursor-pointer"
->
-  Resume
-  <span
-    aria-hidden="true"
-    className="bg-primary absolute inset-0 -translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out z-[-1]"
-  ></span>
-</button>
+        <button
+          type="button"
+          className="border-2 text-primary border-primary p-3 min-w-32 text-center bg-transparent relative overflow-hidden hover:text-white transition-colors hover:z-[2] duration-300 group font-bold cursor-pointer"
+        >
+          Resume
+          <span
+            aria-hidden="true"
+            className="bg-primary absolute inset-0 -translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out z-[-1]"
+          ></span>
+        </button>
         <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
           <GitHubIcon></GitHubIcon>
         </div>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,159 +1,40 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+
 const Projects = () => {
   return (
-    <div className="flex flex-col items-center my-20">
-      <h1 className="text-white text-3xl font-extrabold pt-16 pb-9 ">
+    <div className="flex flex-col items-center my-10 px-4 md:px-0">
+      <h1 className="text-white text-3xl md:text-4xl font-extrabold pt-10 pb-6">
         PROJECTS
       </h1>
-      <div className="text-white  w-[70vw] grid grid-cols-3 gap-4 ">
-        <div className="flex flex-col  items-center p-3 py-8 shadow-xl group min-h-40 min-w-80  shadow-slate-800 hover:-translate-y-3 transition duration-300">
-          <p className="text-xl font-extrabold hover:text-primary ">
-            Notes Website
-          </p>
-          <p className="my-2">A website for CSIT notes</p>
-          <ul className="flex flex-row justify-between space-x-4">
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-          </ul>
-          <ul className="flex flex-row justify-between space-x-4 pt-2">
-            <li>
-              <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-                <GitHubIcon></GitHubIcon>
-              </div>
-            </li>
-            <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-              <OpenInNewIcon></OpenInNewIcon>
-            </div>
-          </ul>
-        </div>{" "}
-        <div className="flex flex-col  items-center p-3 py-8 shadow-xl group min-h-40 min-w-80  shadow-slate-800 hover:-translate-y-3 transition duration-300">
-          <p className="text-xl font-extrabold hover:text-primary ">
-            Notes Website
-          </p>
-          <p className="my-2">A website for CSIT notes</p>
-          <ul className="flex flex-row justify-between space-x-4">
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-          </ul>
-          <ul className="flex flex-row justify-between space-x-4 pt-2">
-            <li>
-              <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-                <GitHubIcon></GitHubIcon>
-              </div>
-            </li>
-            <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-              <OpenInNewIcon></OpenInNewIcon>
-            </div>
-          </ul>
-        </div>{" "}
-        <div className="flex flex-col  items-center p-3 py-8 shadow-xl group min-h-40 min-w-80  shadow-slate-800 hover:-translate-y-3 transition duration-300">
-          <p className="text-xl font-extrabold hover:text-primary ">
-            Notes Website
-          </p>
-          <p className="my-2">A website for CSIT notes</p>
-          <ul className="flex flex-row justify-between space-x-4">
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-          </ul>
-          <ul className="flex flex-row justify-between space-x-4 pt-2">
-            <li>
-              <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-                <GitHubIcon></GitHubIcon>
-              </div>
-            </li>
-            <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-              <OpenInNewIcon></OpenInNewIcon>
-            </div>
-          </ul>
-        </div>{" "}
-        <div className="flex flex-col  items-center p-3 py-8 shadow-xl group min-h-40 min-w-80  shadow-slate-800 hover:-translate-y-3 transition duration-300">
-          <p className="text-xl font-extrabold hover:text-primary ">
-            Notes Website
-          </p>
-          <p className="my-2">A website for CSIT notes</p>
-          <ul className="flex flex-row justify-between space-x-4">
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-          </ul>
-          <ul className="flex flex-row justify-between space-x-4 pt-2">
-            <li>
-              <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-                <GitHubIcon></GitHubIcon>
-              </div>
-            </li>
-            <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-              <OpenInNewIcon></OpenInNewIcon>
-            </div>
-          </ul>
-        </div>{" "}
-        <div className="flex flex-col  items-center p-3 py-8 shadow-xl group min-h-40 min-w-80  shadow-slate-800 hover:-translate-y-3 transition duration-300">
-          <p className="text-xl font-extrabold hover:text-primary ">
-            Notes Website
-          </p>
-          <p className="my-2">A website for CSIT notes</p>
-          <ul className="flex flex-row justify-between space-x-4">
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-          </ul>
-          <ul className="flex flex-row justify-between space-x-4 pt-2">
-            <li>
-              <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-                <GitHubIcon></GitHubIcon>
-              </div>
-            </li>
-            <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-              <OpenInNewIcon></OpenInNewIcon>
-            </div>
-          </ul>
-        </div>{" "}
-        <div className="flex flex-col  items-center p-3 py-8 shadow-xl group min-h-40 min-w-80  shadow-slate-800 hover:-translate-y-3 transition duration-300">
-          <p className="text-xl font-extrabold hover:text-primary ">
-            Notes Website
-          </p>
-          <p className="my-2">A website for CSIT notes</p>
-          <ul className="flex flex-row justify-between space-x-4">
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-          </ul>
-          <ul className="flex flex-row justify-between space-x-4 pt-2">
-            <li>
-              <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-                <GitHubIcon></GitHubIcon>
-              </div>
-            </li>
-            <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-              <OpenInNewIcon></OpenInNewIcon>
-            </div>
-          </ul>
-        </div>{" "}
-        <div className="flex flex-col  items-center p-3 py-8 shadow-xl group min-h-40 min-w-80  shadow-slate-800 hover:-translate-y-3 transition duration-300">
-          <p className="text-xl font-extrabold hover:text-primary ">
-            Notes Website
-          </p>
-          <p className="my-2">A website for CSIT notes</p>
-          <ul className="flex flex-row justify-between space-x-4">
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-          </ul>
-          <ul className="flex flex-row justify-between space-x-4 pt-2">
-            <li>
-              <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-                <GitHubIcon></GitHubIcon>
-              </div>
-            </li>
-            <div className="hover:text-primary relative  after:bg-primary after:absolute after:h-1 after:w-0  after:-bottom-3 after:left-0 hover:after:w-full after:transition-all  after:duration-300  cursor-pointer">
-              <OpenInNewIcon></OpenInNewIcon>
-            </div>
-          </ul>
-        </div>
+      <div className="text-white w-full md:w-[70vw] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {/* Project Card */}
+        {Array.from({ length: 6 }).map((_, idx) => (
+          <div
+            key={idx}
+            className="flex flex-col items-center p-4 py-6 shadow-xl group min-h-40 bg-black shadow-slate-800 hover:-translate-y-3 transition duration-300"
+          >
+            <p className="text-lg md:text-xl font-extrabold hover:text-primary">
+              Notes Website
+            </p>
+            <p className="my-2 text-sm md:text-base">
+              A website for CSIT notes
+            </p>
+            <ul className="flex flex-row justify-between space-x-2 md:space-x-4 text-sm">
+              <li>html</li>
+              <li>css</li>
+              <li>js</li>
+            </ul>
+            <ul className="flex flex-row justify-between space-x-2 md:space-x-4 pt-4">
+              <li className="hover:text-primary cursor-pointer">
+                <GitHubIcon />
+              </li>
+              <li className="hover:text-primary cursor-pointer">
+                <OpenInNewIcon />
+              </li>
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
